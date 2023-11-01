c246e0eeafa09307f5fa96f52b2157e9d667a2b1
Nikhil569 <agarwalnikhil569@gmail.com>
2023-11-01 18:43:46 +0530

    Completed all the functions

--- ip-address-tracker-master/script.js
+++ ip-address-tracker-master/script.js
@@ -1,11 +1,44 @@
-var map = L.map("map").setView([51.505, -0.09], 13);
+const btn = document.getElementsByClassName("ip-search-button")[0];
+const ip = document.getElementsByTagName("input")[0];
+const ipLoc = document
+  .getElementsByClassName("ip-location")[0]
+  .getElementsByTagName("h3")[0];
+const loc = document
+  .getElementsByClassName("location")[0]
+  .getElementsByTagName("h3")[0];
+const timezone = document
+  .getElementsByClassName("timezone")[0]
+  .getElementsByTagName("h3")[0];
+const isp = document
+  .getElementsByClassName("isp")[0]
+  .getElementsByTagName("h3")[0];
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+let map;
 
-L.marker([51.5, -0.09])
-  .addTo(map)
-  .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-  .openPopup();
+btn.addEventListener("click", async () => {
+  let res = await fetch();
+
+  let data = await res.json();
+
+  console.log(data);
+  let lat = data.location.lat;
+  let lng = data.location.lng;
+
+  ipLoc.innerText = data.ip;
+  loc.innerText = data.location.city + ", " + data.location.region;
+  timezone.innerText = "UTC " + data.location.timezone;
+  isp.innerText = data.isp;
+
+  //   map = L.map("map").setView([lat, lng], 13);
+  map =
+    map === undefined
+      ? L.map("map").setView([lat, lng], 13)
+      : map.setView([lat, lng], 13);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker([lat, lng]).addTo(map);
+});
